669381c6f858e951567034b387f26078d3c7de9d
fenglin <510111074@qq.com>
2017-05-10 12:02:20 +0800

    2017/5/9 int版本

--- ruleSectionInPaper.java
+++ ruleSectionInPaper.java
@@ -13,8 +13,8 @@ public class ruleSectionInPaper {
 
 
     public static void main(String[] args){
-        int capacity=2000;
-        int rule_number=2000;
+        int capacity=200;
+        int rule_number=65930;
         int[] weight= initParameter.initData(rule_number);
         int[] cost=initParameter.initCost(rule_number);//{0, 1, 1, 1, 1, 1, 1,1,1,1,1};
         calRulesByParams(capacity,rule_number,weight,cost);
--- ruleSelection.java
+++ ruleSelection.java
@@ -4,6 +4,7 @@ import com.sun.javafx.css.FontUnits;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Created by fenglin on 2017/3/8.
@@ -165,9 +166,94 @@ public class ruleSelection {
         return result;
     }
 
-
     public static ArrayList<String> selectRulesUsingCoverSetMethod(
-            int[] rule_weight, int[] rule_cost, int switch_volume) {
+            int[] rule_weight, int[] rule_cost, int switch_volume,int N) {
+
+
+
+
+        ArrayList<String> result = new ArrayList<String>();
+
+//        int rule_total_number = rule_cost.length;
+        int[] rule_new_cost = new int[N+1];
+        int[] rule_ratio = new int[N+1];
+        boolean[] rule_deal = new boolean[N+1];
+
+
+        int[] weight_coverSet = new int[N + 1];
+        weight_coverSet[0] = 0;
+        int[] cost_coverSet = new int[N + 1];
+        cost_coverSet[0] = 0;
+        for (int i = 1; i < N + 1; i++) {
+
+            weight_coverSet[i] = rule_weight[i]/2;
+            cost_coverSet[i] = 2;
+            rule_deal[i]=false;
+            rule_ratio[i]=weight_coverSet[i]/cost_coverSet[i];
+
+        }
+
+        rule_weight=weight_coverSet;
+        rule_new_cost=cost_coverSet;
+
+        HashMap<Integer, ArrayList<Integer>> dependency = initParameter.initDependency(N);//initDependency();
+
+
+
+//        for (int i = 0; i < rule_total_number; i++) {
+//            System.out.println(i + " " + rule_ratio[i]);
+//        }
+
+
+        int volume = switch_volume;
+        int max = rule_ratio[0];
+        int pos = -1;
+        while (result.size() < switch_volume) {
+            max = -1;
+            for (int i = 1; i < N+1; i++) {
+                if (rule_deal[i] == false && rule_ratio[i] > max) {
+                    max = rule_ratio[i];
+                    pos = i;
+                }
+            }
+//            System.out.println("this turn choose: "+pos+" "+volume);
+            if (rule_new_cost[pos] <= volume) {
+//                System.out.println("///  "+pos);
+
+
+                    result.add(pos + "");
+                    result.add(pos + "*");
+                    rule_deal[pos] = true;
+
+                volume = volume - rule_new_cost[pos];
+                if (volume == 1) break;
+//                volume+=nn;
+//                System.out.println(volume+ " ----");
+
+
+            } else {
+                rule_deal[pos] = true;
+            }
+
+
+        }
+
+
+        return result;
+
+
+
+
+
+
+
+
+
+
+
+    }
+    public static ArrayList<String> selectRulesUsingCoverSetMethoddd(
+            int[] rule_weight, int[] rule_cost, int switch_volume,int N) {
         ArrayList<String> result = new ArrayList<String>();
 
         int rule_total_number = rule_cost.length;
@@ -175,7 +261,7 @@ public class ruleSelection {
         int[] rule_ratio = new int[rule_total_number];
         boolean[] rule_deal = new boolean[rule_total_number];
 
-        HashMap<Integer, ArrayList<Integer>> dependency = initDependency();
+        HashMap<Integer, ArrayList<Integer>> dependency = initParameter.initDependency(N);//initDependency();
 
         for (int i = 0; i < rule_total_number; i++) {
             rule_new_cost[i] = 2;
@@ -280,18 +366,42 @@ public class ruleSelection {
 
 
     public static ArrayList<Integer> selectRulesUsingDependentSetMethod(
-            int[] rule_weight, int[] rule_cost, int switch_volume) {
+            int[] rule_weight, int[] rule_cost, int switch_volume, int N) {
 
         ArrayList<Integer> result = new ArrayList<Integer>();
 
-        int rule_total_number = rule_weight.length;
-        int[] rule_ratio = new int[rule_total_number];
-        boolean[] rule_deal = new boolean[rule_total_number];
-        HashMap<Integer, ArrayList<Integer>> dependency = initDependency();
+//        int rule_total_number = N;
+        int[] rule_ratio = new int[N+1];
+        boolean[] rule_deal = new boolean[N+1];
+        int[] weight_dependency = new int[N + 1];
+        weight_dependency[0] = 0;
+        int[] cost_dependency = new int[N + 1];
+        cost_dependency[0] = 0;
+        HashMap<Integer, ArrayList<Integer>> dependency = initParameter.initDependency(N);//initDependency();
 
 
-        for (int i = 0; i < rule_total_number; i++) {
-            rule_ratio[i] = rule_weight[i] / rule_cost[i];
+        ArrayList<Integer> templist;//=new ArrayList<Integer>();
+
+        //初始化方法选择的矩阵
+        //方法1：依赖集
+        //方法2：覆盖集
+        for (int i = 1; i < N + 1; i++) {
+            templist = dependency.get(i);
+            int sum_weight = rule_weight[i];
+            int sum_cost = rule_cost[i];
+            for (int n : templist) {
+                sum_weight += rule_weight[n];
+                sum_cost += rule_cost[n];
+            }
+            weight_dependency[i] = sum_weight/sum_cost;
+            cost_dependency[i] = sum_cost;
+
+        }
+
+
+
+        for (int i = 1; i < N+1; i++) {
+            rule_ratio[i] = weight_dependency[i]/rule_cost[i];//rule_weight[i] / rule_cost[i];
             rule_deal[i] = false;
         }
 
@@ -299,12 +409,14 @@ public class ruleSelection {
 //            System.out.println(i+" "+rule_ratio[i]);
 //        }
 
+        rule_deal[0]=true;
+
         int volume = switch_volume;
-        int max = rule_ratio[0];
+        int max = rule_ratio[1];
         int pos = -1;
         while (result.size() < switch_volume) {
             max = -1;
-            for (int i = 0; i < rule_total_number; i++) {
+            for (int i = 1; i < N+1; i++) {
                 if (rule_deal[i] == false && rule_ratio[i] > max) {
                     max = rule_ratio[i];
                     pos = i;
@@ -318,13 +430,13 @@ public class ruleSelection {
                 for (Integer i : dependency.get(pos)) {
 
 //                        System.out.println(result.contains(i)+" "+i);
-                    boolean flag = result.contains(i);
-                    if (!result.contains(i)) {
+//                    boolean flag = result.contains(i);
+//                    if (!result.contains(i)) {
 //
                         result.add(i);
                         rule_deal[i] = true;
                         nu++;
-                    }
+//                    }
 //                    System.out.println("456");
 
                 }
@@ -336,7 +448,7 @@ public class ruleSelection {
                 rule_deal[pos] = true;
             }
 
-
+//            System.out.println(pos+" "+dependency.get(pos)+" "+result);
         }
 
         return result;
@@ -378,45 +490,71 @@ public class ruleSelection {
 
     public static void main(String[] args) {
 
+        int switch_volume = 200;
+        int N=65930;
+        int[] rule_weight = initParameter.initData(N);//{10, 60, 30, 5, 10, 120};
+        int[] rule_cost = initParameter.initCost(N);//{1, 2, 3, 1, 2, 3};
 
-        int[] rule_weight = {10, 60, 30, 5, 10, 120};
-        int[] rule_cost = {1, 2, 3, 1, 2, 3};
-        int switch_volume = 4;
+/*
+        //覆盖集方法的代码
+        ArrayList<String> result=selectRulesUsingCoverSetMethod(rule_weight,rule_cost,switch_volume,N);
 
-        ArrayList<Integer> rule1 = selectRulesUsingDependentSetMethod(rule_weight, rule_cost, switch_volume);
-
+        HashSet<String> set=new HashSet<String>();
         int sum = 0;
-        for (int i : rule1) {
-            System.out.print(i + " ");
-            sum += rule_weight[i];
+        for (String i : result) {
+//            System.out.print(i + " ");
+            set.add(i);
         }
+//        System.out.println(result);
+//        System.out.println(set);
+        System.out.println(set.size());
+//        System.out.println("Mixed Set : " + sum);
 
-        System.out.println("Dependecy Set : " + sum);
 
-        sum = 0;
 
-        ArrayList<String> rule2 = selectRulesUsingCoverSetMethod(rule_weight, rule_cost, switch_volume);
-
-        for (String i : rule2) {
-            System.out.print(i + " ");
+        int sum2 = 0;
+        for (String i : set) {
+//            System.out.print(i + " ");
+            set.add(i);
             if (!i.contains("*")) {
-                sum += rule_weight[Integer.parseInt(i)];
+                sum2 += rule_weight[Integer.parseInt(i)];
 
             }
         }
 
-        System.out.println("Cover Set : " + sum);
+        System.out.println("cover set weight:"+sum2);
 
-        ArrayList<String> rule3 = selectRuleUsingMixedSetMethod(rule_weight, rule_cost, switch_volume);
-        sum = 0;
-        for (String i : rule3) {
-            System.out.print(i + " ");
-            if (!i.contains("*")) {
-                sum += rule_weight[Integer.parseInt(i)];
-            }
+
+
+*/
+
+
+      //  依赖集方法的代码
+        ArrayList<Integer> result=selectRulesUsingDependentSetMethod(rule_weight,rule_cost,switch_volume,N);
+
+        HashSet<Integer> set=new HashSet<Integer>();
+        int sum = 0;
+        for (Integer i : result) {
+//            System.out.print(i + " ");
+            set.add(i);
         }
+//        System.out.println(result);
+//        System.out.println(set);
+        System.out.println(set.size());
+//        System.out.println("Mixed Set : " + sum);
 
-        System.out.println("Mixed Set : " + sum);
+        int sum2 = 0;
+        for (Integer i : set) {
+//            System.out.print(i + " ");
+            set.add(i);
+
+                sum2 += rule_weight[i];
+
+
+        }
+        System.out.println("dependency set weight:"+sum2);
+
+
 
 
     }
